519ef454f9d60cce9956195c8832e623b801da63
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2026-06-02 19:48:34 +0000

    pinning versions for better behavior

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import { resolve } from "node:path";
-import { container, image } from "../programmatic-docker-suede";
-import { devcontainerNetwork } from "../programmatic-docker-suede/devcontainer.js";
+import {
+  container,
+  image,
+} from "../browser-control-container-suede.programmatic-docker-suede";
+import devcontainer from "../browser-control-container-suede.programmatic-docker-suede/devcontainer.js";
 import CommandStream, {
   type CompletedResult,
-} from "../programmatic-docker-suede/CommandStream.js";
+} from "../browser-control-container-suede.programmatic-docker-suede/CommandStream.js";
 import defaults from "./defaults.js";
 
 /**
@@ -59,7 +62,7 @@ export const buildAndRun = async (BROWSER: Browser, details?: Options) => {
   if (exit !== 0)
     throw new Error(`Build failed for ${tag} with error:\n${err}`);
 
-  const network = details?.network ?? (await devcontainerNetwork());
+  const network = details?.network ?? (await devcontainer.network());
 
   const command = details?.command ?? defaults.command;
   return container.run({ network, name, command, image: tag });
